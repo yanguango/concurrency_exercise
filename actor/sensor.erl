c2362d39497cfcdc8dd3c81c3c1a716a043c1bf2
yanguango <yanguango@gmail.com>
2014-11-16 00:18:59 -0500

    fix watcher/sensor assignment

--- actor/sensor.erl
+++ actor/sensor.erl
@@ -1,18 +1,23 @@
 -module(sensor).
 -export([measure/2]).
 
+%% if measurement is 11, sensor should exit
+%% and send message to watcher
 handle(_, Sensor_ID, 11) ->
   exit({anomalous_reading, Sensor_ID});
 
+%% if measurement is 1-10, send it to watcher
 handle(Watcher,Sensor_ID,  Measurement) -> 
   Watcher ! {Sensor_ID, Measurement}.
 
+%% measure process, generate random measurement between 1 and 11
+%% handle the measure and sleep for a random number of milliseconds
 measure(Watcher, Sensor_ID) ->
   random:seed(erlang:now()),
+  Sleep_time = random:uniform(10000),
+  timer:sleep(Sleep_time),
   Measurement = random:uniform(11),
   handle(Watcher, Sensor_ID,  Measurement),
-  Sleep_time = random:uniform(1000),
-  timer:sleep(Sleep_time),
   measure(Watcher, Sensor_ID).
   
 
--- actor/watcher.erl
+++ actor/watcher.erl
@@ -1,16 +1,19 @@
 -module(watcher).
--export([watch/1, watcher_start/3, start/0, main/1]).
+-export([watch/1, watcher_start/3, start/0]).
 
+%% start watcher then start its sensors
 watcher_start(N, End_ID, Sensor_list) ->
   if
     N =< End_ID ->
       {Sensor_PID, _} = spawn_monitor(sensor, measure, [self(), N]),
       watcher_start(N + 1, End_ID, Sensor_list ++ [{N, Sensor_PID}]);
     true ->
+      io:fwrite("Watcher starts, initial list of sensors: ~p~n", [Sensor_list]),
       watch(Sensor_list)
   end.
 
-
+%% watch loop, receive measurements and exit message
+%% if sensor exit, watcher will restart the sensor with same ID
 watch(Sensor_list) ->
   receive
     {Sensor, Measurement} ->
@@ -21,10 +24,12 @@ watch(Sensor_list) ->
       Filtered_sensor_list = lists:filter(fun({Sid, _}) -> Sensor_ID /= Sid end, Sensor_list),
       {Sensor_PID, _} = spawn_monitor(sensor, measure, [self(), Sensor_ID]),
       Updated_sensor_list = Filtered_sensor_list ++ [{Sensor_ID, Sensor_PID}],
-      io:fwrite("Sensor restart, updated list: ~p~n", [Updated_sensor_list]),
+      io:fwrite("Sensor restarts, updated list: ~p~n", [Updated_sensor_list]),
       watch(Updated_sensor_list)
   end.
 
+%% assign sensor IDs to watcher
+%% every watcher watch at most 10 sensors
 distribute(Start_ID, End_ID) ->
   if
     Start_ID + 9 =< End_ID ->
@@ -34,13 +39,16 @@ distribute(Start_ID, End_ID) ->
       spawn(watcher, watcher_start, [Start_ID, End_ID, []])
   end.
 
+%% sensors distribution entrance function
+distribute(Sensor_num) ->
+  distribute(0, Sensor_num - 1).
+
+%% read number of sensors from standard input
 start() ->
   {ok, [Sensor_num]} = io:fread("Enter number of sensors> ", "~d"),
-  distribute(0, Sensor_num),
+  distribute(Sensor_num),
   receive
     done -> 
       ok
   end.
 
-main(_) ->
-  start().
